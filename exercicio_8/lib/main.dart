4b5b8a8bdd2c8ea35ce3c2d945cb97d9b27d5754
Wagner Lemos Duarte <wagner.duarte@cwi.com.br>
2020-09-02 22:43:14 -0300

    adicionado dotenv

--- exercicio_8/lib/main.dart
+++ exercicio_8/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:exercicio7start/ui/detail.page.dart';
 import 'package:exercicio7start/ui/home.page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future main() async {
+  await DotEnv().load('.env');
   runApp(MyApp());
 }
 
--- exercicio_8/lib/network/api.dart
+++ exercicio_8/lib/network/api.dart
@@ -1,13 +1,14 @@
 import 'package:exercicio7start/models/news.response.dart';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Api {
   static Future<NewsResponse> retrieveLocalNews(BuildContext context) async {
     Dio dio = Dio();
     dio.options.baseUrl = 'https://api.currentsapi.services/';
 
-    Response response = await dio.get('v1/latest-news?apiKey=YcIsZ0zdsQPIBeCptBhTTHcRvinK1DzwaoAQE61uelNT1nbu');
+    Response response = await dio.get('v1/latest-news?apiKey=${DotEnv().env['API_KEY']}');
 
     return NewsResponse.fromJson(response.data);
   }
